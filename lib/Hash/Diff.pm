58e13a5b335b988b23146fba14df30e4178eb2e4
Bjørn-Olav Strand <bolav@cpan.org>
2010-01-28 14:16:03 +0000

    Import of BOLAV/Hash-Diff-0.002 from CPAN.
    gitpan-cpan-distribution: Hash-Diff
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         BOLAV/Hash-Diff-0.002.tar.gz
    gitpan-cpan-author:       BOLAV
    gitpan-cpan-maturity:     released

---
+++ lib/Hash/Diff.pm
@@ -0,0 +1,106 @@
+package Hash::Diff;
+
+use strict;
+use warnings;
+use Carp;
+use Hash::Merge;
+
+use base 'Exporter';
+use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = "0.002";
+@EXPORT_OK   = qw( diff left_diff );
+
+sub left_diff {
+    my ($h1, $h2) = @_;
+    my $rh = {};
+    
+    foreach my $k (keys %{$h1}) {
+        if (ref $h1->{$k} eq 'HASH') {
+            if (ref $h2->{$k} eq 'HASH') {
+                $rh->{$k} = left_diff($h1->{$k}, $h2->{$k});
+            }
+            else {
+                $rh->{$k} = $h1->{$k}                
+            }
+        }
+        elsif ((!defined $h2->{$k})||($h1->{$k} ne $h2->{$k})) {
+            $rh->{$k} = $h1->{$k}
+        }
+    }
+    
+    return $rh;
+
+}
+
+sub diff {
+    my ($h1, $h2) = @_;
+
+    return Hash::Merge::merge(left_diff($h1,$h2),left_diff($h2,$h1));
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Hash::Diff - Return difference between to hashes as a hash
+
+=head1 SYNOPSIS
+
+    use Hash::Diff qw( diff );
+    my %a = ( 
+		'foo'    => 1,
+	    'bar'    => { a => 1, b => 1 },
+	);
+    my %b = ( 
+		'foo'     => 2, 
+		'bar'    => { a => 1 },
+	);
+
+    my %c = %{ diff( \%a, \%b ) };
+    
+    # %c = %{ foo => 1, bar => { b => 1} }
+
+=head1 DESCRIPTION
+
+Hash::Diff returns the diffrence between to hashes as a hash.
+
+=over 
+
+=item diff ( <hashref>, <hashref> )
+
+Diffs two hashes.  Returns a reference to the new hash.
+
+=item left_diff ( <hashref>, <hashref> )
+
+Returns the values in the left hash that is not, or diffrent from the right hash.
+
+
+=head1 CAVEATS
+
+This will not handle self-referencing/recursion within hashes well.  
+This will only handle HASH and SCALAR. 
+
+Plans for a future version include incorporate deep recursion protection.
+And support for ARRAY.
+
+=head1 BUGS
+
+Sure!
+Report here: http://rt.cpan.org/NoAuth/Bugs.html?Dist=Hash::Diff
+
+=head1 AUTHOR
+
+Bjorn-Olav Strand E<lt>bo@startsiden.noE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2010 ABC Startsiden AS. All rights reserved.
+
+This library is free software.  You can redistribute it and/or modify it 
+under the same terms as Perl itself.
+
+=cut
